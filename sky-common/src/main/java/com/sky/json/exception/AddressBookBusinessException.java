67f4df689feecb638dfbbe8a09fd4e19d720fd6a
likkjxx <2326076511@qq.com>
2024-03-05 18:13:43 +0800

    Initial commit

---
+++ sky-common/src/main/java/com/sky/json/exception/AddressBookBusinessException.java
@@ -0,0 +1,9 @@
+package com.sky.json.exception;
+
+public class AddressBookBusinessException extends BaseException {
+
+    public AddressBookBusinessException(String msg) {
+        super(msg);
+    }
+
+}
