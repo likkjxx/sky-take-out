719c2e71ed45e1bb869d65651000d0d38e7ce2c8
likkjxx <2326076511@qq.com>
2024-03-07 17:07:11 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -88,9 +88,16 @@ public class EmployeeController {
     @GetMapping("/page")
     @ApiOperation("分页查询")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
-        log.info("{}",employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    @PostMapping("status/{status}")
+    @ApiOperation("员工禁用启用操作")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("{},{}",status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
     }
 
 }
