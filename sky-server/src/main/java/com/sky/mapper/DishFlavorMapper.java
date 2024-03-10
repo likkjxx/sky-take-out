ebd15ab11fa4a3f777bf12434e6277396ab91c7f
likkjxx <2326076511@qq.com>
2024-03-10 11:10:28 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Zz
+ * @version 1.0
+ * @description:
+ * @date 2024/3/10 10:49
+ */
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+}
